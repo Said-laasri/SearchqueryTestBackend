dba441babcf363368cb47e0df9d54d87b2afe9c8
said-laasri <saidlaasri3@gmail.com>
2025-04-13 12:49:57 +0100

    Api/index vercel

--- api/index.js
+++ api/index.js
@@ -1,11 +1,25 @@
 const express = require('express');
-const searchRoute = require('../routes/search');
-const db = require('../db');
-const serverless = require('serverless-http');
+const searchRoute = require('./routes/search');
+const db = require('./db');
+require('dotenv').config();
 const cors = require('cors');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+
 app.use(cors());
 app.use('/api/search', searchRoute);
 
-module.exports.handler = serverless(app);
+(async () => {
+  const isConnected = await db.checkConnection();
+  if (!isConnected) {
+    console.error('❌ Could not connect to database. Exiting...');
+    process.exit(1);
+  } else {
+    console.log('✅ Database connected successfully!');
+  }
+
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+})();
